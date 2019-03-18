4fb6b3c2be3fd6a4d21b9c74563cdaa45fad2844
Junaid Malik <junaid.malik@n2o.co.uk>
2019-03-18 14:55:30 +0000

    api auth repository

---
+++ SocialApp.API/Authorization/AuthRepository.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialApp.API.Data;
+using SocialApp.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace SocialApp.API.Authorization
+{
+    public class AuthRepository : IAuthRepository
+    {
+        public readonly AppDataContext dataContext;
+
+        public AuthRepository(AppDataContext context)
+        {
+            dataContext = context;
+        }
+
+        public async Task<User> Login(string username, string password)
+        {
+            var user = await dataContext.Users
+                                        .FirstOrDefaultAsync(c => c.Username == username);
+
+            if(user == null)
+            {
+                return null;
+            }
+
+            if(!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                return null;
+            }
+
+            return user;
+        }
+
+
+        public async Task<User> Register(User user, string password)
+        {
+            byte[] passwordHash, passwordSalt;
+
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await dataContext.Users.AddAsync(user);
+            await dataContext.SaveChangesAsync();
+
+            return user;
+        }
+
+
+        public async Task<bool> UserExists(string username)
+        {
+            if(await dataContext.Users.AnyAsync(c => c.Username == username))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if(computedHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+    }
+}
---
+++ SocialApp.API/Data/IAuthRepository.cs
@@ -0,0 +1,17 @@
+﻿using SocialApp.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialApp.API.Data
+{
+    public interface IAuthRepository
+    {
+        Task<User> Register(User user, string password);
+
+        Task<User> Login(string username, string password);
+
+        Task<bool> UserExists(string username);
+    }
+}
