d0abe5452e928239f42ca84032105920551e67c3
Junaid Malik <junaid.malik@n2o.co.uk>
2019-03-19 10:45:42 +0000

    removed values ng component

--- SocialApp-SPA/src/app/app.module.ts
+++ SocialApp-SPA/src/app/app.module.ts
@@ -3,31 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
 
 import {MenubarModule} from 'primeng/menubar';
 import {MenuItem} from 'primeng/api';
 import { AuthService } from './services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent,
       NavComponent,
+      HomeComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       MenubarModule,
-      FormsModule,
+      FormsModule
    ],
    providers: [
       AuthService
    ],
    bootstrap: [
-      AppComponent,
+      AppComponent
    ]
 })
 export class AppModule { }
--- SocialApp.API/Controllers/ValuesController.cs
+++ SocialApp.API/Controllers/ValuesController.cs
@@ -23,7 +23,7 @@ namespace SocialApp.API.Controllers
             DataContext = dataContext;
         }
 
-        // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
