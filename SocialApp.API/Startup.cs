606667491f6ff964dbafe70335a4bc99e10e0287
Junaid Malik <junaid.malik@n2o.co.uk>
2019-03-20 12:48:34 +0000

    automapper import, startup repo config

--- SocialApp.API/Startup.cs
+++ SocialApp.API/Startup.cs
@@ -34,6 +34,8 @@ namespace SocialApp.API
 
             services.AddScoped<IAuthRepository, AuthRepository>();
 
+            services.AddScoped<ISocialRepository, SocialRepository>();
+
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
                 {
@@ -86,7 +88,11 @@ namespace SocialApp.API
                 });
             }
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
