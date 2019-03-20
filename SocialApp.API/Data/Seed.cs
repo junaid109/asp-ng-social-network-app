924b174a3e198f10dceb764bff781027992a72ae
Junaid Malik <junaid.malik@n2o.co.uk>
2019-03-20 10:57:25 +0000

    seed and migration data

---
+++ SocialApp.API/Data/Seed.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using SocialApp.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace SocialApp.API.Data
+{
+    public class Seed
+    {
+        private readonly AppDataContext _appData;
+
+        public Seed(AppDataContext appDataContext)
+        {
+            _appData = appDataContext;
+        }
+
+        public void SeedUsers()
+        {
+            var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
+            var users = JsonConvert.DeserializeObject<List<User>>(userData);
+
+            foreach (var user in users)
+            {
+                byte[] passwordHash, passwordSalt;
+
+                CreatePasswordHash("Password", out passwordHash, out passwordSalt);
+
+                user.PasswordHash = passwordHash;
+                user.PasswordSalt = passwordSalt;
+                user.Username = user.Username.ToLower();
+
+                _appData.Users.Add(user);
+            }
+
+            _appData.SaveChanges();
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
---
+++ SocialApp.API/Models/Photo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialApp.API.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+
+        public string Url { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public bool IsMainPhoto { get; set; }
+
+        public User User { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
