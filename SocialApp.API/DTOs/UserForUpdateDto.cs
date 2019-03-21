926d9530b12d1dbb8c5ce487a06d7d62ea3436c5
Junaid Malik <junaid.malik@n2o.co.uk>
2019-03-21 14:53:32 +0000

    user update dto

---
+++ SocialApp.API/DTOs/UserForUpdateDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialApp.API.DTOs
+{
+    public class UserForUpdateDto
+    {
+        public string Introduction { get; set; }
+
+        public string Interests { get; set; }
+
+        public string Hobbies { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+    }
+}
